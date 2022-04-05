517d47c1016708635c64b58ced8e286abbcf581e
Siyuan <sy00010@mix.wvu.edu>
2022-04-05 19:55:14 -0400

    add authenticationServive

---
+++ src/app/auth/authentication/authentication.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+    providedIn:'root'
+})
+export class AuthenticationService{
+    baseUrl:string="https://identitytoolkit.googleapis.com/vl/accounts";
+    SignUpEndPoint:string="signUp";
+
+    public constructor(private http:HttpClient){
+
+    }
+
+    public signUp(email:string,password:string){
+        const requestBody = {
+            "email": email,
+            "password": password,
+            "returnSecureToken":true
+        };
+        return this.http.post(this.baseUrl + ":" + this.SignUpEndPoint + "?" + "key=" + environment.firebase.apiKey, requestBody);
+    }
+
+}
