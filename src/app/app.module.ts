02e7a5dcdb1f48aad5824d61bf434e28f0778ad9
Siyuan <sy00010@mix.wvu.edu>
2022-04-07 16:31:13 -0400

    clean module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireDatabase, AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from '../environments/environment';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideDatabase, getDatabase } from '@angular/fire/database';
@@ -41,7 +41,6 @@ import { AuthenticationComponent } from './auth/authentication/authentication.co
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireDatabase,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
